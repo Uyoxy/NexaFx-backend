76cbf89db76117216ed83a955e101b8f445d258b
nafiuishaaq <nafiuig@gmail.com>
2025-03-22 22:57:42 +0100

    updated the service file

--- src/currencies/currencies.service.ts
+++ src/currencies/currencies.service.ts
@@ -1,43 +1,5 @@
-import { Injectable, NotFoundException, ConflictException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Currency } from './entities/currency.entity';
-import { CreateCurrencyDto } from './dto/create-currency.dto';
-
+import { Injectable } from '@nestjs/common';
 @Injectable()
 export class CurrenciesService {
-  constructor(
-    @InjectRepository(Currency)
-    private currencyRepository: Repository<Currency>,
-  ) {}
 
-  async create(createCurrencyDto: CreateCurrencyDto): Promise<Currency> {
-    const existingCurrency = await this.currencyRepository.findOne({ where: { code: createCurrencyDto.code } });
-    if (existingCurrency) {
-      throw new ConflictException('Currency code already exists');
-    }
-
-    const currency = this.currencyRepository.create(createCurrencyDto);
-    return this.currencyRepository.save(currency);
-  }
-
-  async findAll(): Promise<Currency[]> {
-    return this.currencyRepository.find();
-  }
-
-  async findOne(id: string): Promise<Currency> { // Ensure id is of type string
-    const currency = await this.currencyRepository.findOne({ where: { id } });
-    if (!currency) {
-      throw new NotFoundException('Currency not found');
-    }
-    return currency;
-  }
-  
-  async remove(id: string): Promise<void> { // Ensure id is of type string
-    const result = await this.currencyRepository.delete(id);
-    if (result.affected === 0) {
-      throw new NotFoundException('Currency not found');
-    }
-  }
-  
 }
